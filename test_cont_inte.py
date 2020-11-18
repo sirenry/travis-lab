d689c552f604afbd34f95420c03da34c26dba561
Sir Henry <72149952+sirenry@users.noreply.github.com>
2020-11-18 20:50:13 +0000

    Initial commit for test progrma

---
+++ test_cont_inte.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# --------------------------------------------------------------------------------------
+# Course: Programming Skills - Online INFD11016 Sep-Dec 2020
+# Course organiser: Dr Chris Wood.
+#
+# ID of practical: Continuous integration and Travis CI practical
+# Name of student: Enrique Mendez, e.mendez@sms.ed.ac.uk
+# Details about practical: Here, we will set up a first Travis CI job, based around a
+# simple program example, to get you up and running with Travis CI This is not intended
+# to be a complete introduction to everything that Travis CI offers.
+# Rather, it is hoped that it will serve to get you started, helping you through your
+# first 20 minutes of using Travis CI, to give an idea as to its potential and
+# usefulness.
+#
+# References and and Acknowledgements
+# * Jackson, Mike (24 Oct 2018).  EPCC, The University of Edi.
+# --------------------------------------------------------------------------------------
+#
+# COMMENTS ON PROGRAM
+#
+# This program is for testing purpuses and see how it works in travis CI.
+# ----------------------------------
+
+def test_firsttestcase():
+    assert 1 + 2 == 3
+
+def test_secondtestcase():
+    assert [1, 2] != [1, 2, 3]
